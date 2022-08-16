ac7d9b912134c067c4445fc598d84d57b1e10fc7
Vincent Delaleu <12876957+delvvince@users.noreply.github.com>
2022-08-16 07:59:22 +0200

    change greeting text for packagist

--- src/Arxitall.php
+++ src/Arxitall.php
@@ -2,12 +2,10 @@
 
 namespace ArgilData\Arxitall;
 
-
-
 class Greetr
 {
     public function greet(String $sName)
     {
-        return 'Hi ' . $sName . '! How are you doing from Arxitall on packagist?';
+        return 'Hi ' . $sName . '! How are you doing in composer?';
     }
 }
