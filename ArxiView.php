8931b455cd58ad04d4ac4c7035e024c9d820110b
Vincent Delaleu <12876957+delvvince@users.noreply.github.com>
2022-10-25 20:27:00 +0200

    hotfix for ServiceProvider Class not found

--- ArxiView.php
+++ ArxiView.php
@@ -4,6 +4,7 @@ namespace ArgilData\ArxiView;
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
+use ArgilData\ArxiView\Providers\VuiKitServiceProvider;
 use ArgilData\ArxiView\View\Components;
 
 class ArxiView
