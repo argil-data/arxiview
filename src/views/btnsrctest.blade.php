44f46834480cb061f6fea1321c1483c08836f3a1
Vincent Delaleu <12876957+delvvince@users.noreply.github.com>
2022-11-25 16:33:15 +0100

    change default btnsrctest style

--- src/views/btnsrctest.blade.php
+++ src/views/btnsrctest.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <button class="border-2 border-black py-2 px-4 hover:bg-main hover:text-white"
+    <button class="border-2 border-black hover:border-teal-500 py-2 px-4 hover:bg-gray-100 hover:text-teal-500"
     {{-- {{ $attributes->class(['bn bn-'.$size.' bn-'.$color])->merge(['type' => 'button']) }} --}}
     >
         button from src/views/   
